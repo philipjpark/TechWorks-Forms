772389e2aa64fb285d0e14d8b5302d71e6fe06af
philipjpark <66290921+philipjpark@users.noreply.github.com>
2020-06-29 15:17:23 -0400

    Forms workshop completed.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, {Component} from 'react';
+class Forms extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+    }
+  }
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+    console.log(event.target.name);
+  }
+  handleSubmit = (event) => {
+    // event.preventDefault(); // this is stop propagation
+    alert(`an input was submitted: UserName: ${this.state.username} Password: ${this.state.password}`)
+  }
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="name">
+          User Name:
+          <input name="username" onChange={this.handleChange} value={this.state.username} type="text" placeholder="Put your name here" />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+        </label>
+        <input type="submit" />
+      </form>
+    )
+  };
+};
+export default Forms;
